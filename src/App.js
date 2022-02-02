96108ae9ca3ce5b65666f698c00df1bae994999e
KaviKurtosys <95690055+KaviKurtosys@users.noreply.github.com>
2022-02-02 10:35:30 +0200

    Animate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { motion } from 'framer-motion';
+import { useRef, useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <motion.h1 animate={{ x: 50}}>Hello</motion.h1>
     </div>
   );
 }
